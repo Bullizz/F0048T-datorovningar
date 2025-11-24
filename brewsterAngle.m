56f1c94203772333dea26d1aa7419fe00b7516e1
Albin <80341227+Bullizz@users.noreply.github.com>
2025-11-24 20:42:25 +0100

    brewster

---
+++ brewsterAngle.m
@@ -0,0 +1,96 @@
+function brewsterAngle()
+    % Parametrar till beräkningarna
+    nLuft       = 1;
+    nGlas       = 1.6;
+    thetaLuft   = atand(nGlas / nLuft); % Brewster vinkel
+    lambda      = 1e-6;
+    alfaLuft    = 0;
+    alfaGlas    = 0;
+    c           = 3e8;
+    
+    % Kant vektorerna för beräknings områderna
+    y           = linspace(0, 10e-6, 800);
+    zLuft       = linspace(-5e-6, 0, 400);
+    zGlas       = linspace(0, 5e-6, 400);
+    
+    % Definera riktningsvektorn för vågen i luft
+    sLuft       = [sind(thetaLuft), cosd(thetaLuft)];
+    
+    % Definera riktningsvektorn för vågen i glas
+    thetaGlas   = asind(sind(thetaLuft) * (nLuft / nGlas));
+    sGlas       = [sind(thetaGlas), cosd(thetaGlas)];
+    
+    % Generera planen i luft och glas
+    ULuft       = planewave(y, zLuft, sLuft, lambda / nLuft, alfaLuft);
+    UGlas       = planewave(y, zGlas, sGlas, lambda / nGlas, alfaGlas);
+    
+    % Skala vågen i glas
+    [tp]    = TransCoef(sLuft, sGlas, nLuft, nGlas);
+    UGlas   = UGlas .* tp;
+    
+    % Slå samman de två vågorna
+    U       = [ULuft, UGlas];
+    
+    % Beräkna itensiteterna
+    ILuft = abs(ULuft) .^ 2;
+    IGlas = abs(UGlas) .^ 2;
+    
+    % Beräkna energitättheterna
+    WLuft   = (ILuft .* nLuft .* sLuft(2)) / c;
+    WGlas   = (IGlas * nGlas .* sGlas(2)) / c;
+    
+    % Slå samman energitätheterna
+    W = [WLuft, WGlas];
+    
+    %%%
+    % Genererera resultat figurer
+    %%%
+    % Beräkna fas och intensitet
+    Fas     = angle(U);
+    I       = abs(U) .^ 2;
+    Wsnitt  = W(500, :) * 1e9;
+    Isnitt  = I(500, :);
+    
+    % Fixa vektorer till x och y axlar till figuren
+    zAxis = [zLuft, zGlas] * 1e6;
+    yAxis = y * 1e6;
+    
+    % Fasen
+    figure(1);
+        imagesc(zAxis, yAxis, Fas);
+        colormap('gray');
+        Chdl = colorbar;
+        set(Chdl.Title, 'String', '[-\pi,\pi]');
+        axis xy;
+        axis square;
+        title('Phase', 'fontsize', 18);
+        xlabel('z [\mum]', 'fontsize', 16);
+        ylabel('y [\mum]', 'fontsize', 16);
+    
+    % Itensiteten
+    figure(2);
+        imagesc(zAxis, yAxis, I);
+        colormap('gray');
+        Chdl = colorbar;
+        set(Chdl.Title, 'String', '[W/m^2]');
+        axis xy;
+        axis square;
+        title('Intensity', 'fontsize', 18);
+        xlabel('z [\mum]','fontsize',16);
+        ylabel('y [\mum]','fontsize',16);
+    
+    % Tvärsnitt av intensiteten och energitätheten
+    figure(3);
+        subplot(1, 2, 1);
+            plot(zAxis, Isnitt, 'linewidth', 2);
+            title('Intensitet', 'fontsize', 18);
+            xlabel('z [\mum]', 'fontsize', 16);
+            ylabel('Itensitet [W/m^2]', 'fontsize', 16);
+        
+        subplot(1, 2, 2);
+            ax = plot(zAxis, Wsnitt, 'linewidth', 2);
+            title('Energitäthet', 'fontsize', 18);
+            xlabel('z [\mum]', 'fontsize', 16);
+            ylabel('Energitäthet [n J/m^3]', 'fontsize', 16);
+            ytickformat('%,.3f');
+end
