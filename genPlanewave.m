631a8e261e280b0eba8edb07ae62696eacd32de7
Albin <80341227+Bullizz@users.noreply.github.com>
2025-11-24 17:01:06 +0100

    Added skala-intensiteter and global planewave function

--- genPlanewave.m
+++ genPlanewave.m
@@ -31,28 +31,3 @@ function genPlanewave()
     axis xy
     axis off
 end
-
-function [U] = planewave(y,z,s,lambda,alfa)
-    % U = planewave(y,z,s,lambda)
-    % This function generates the complex amplitude for a plane wave.
-    % Input:
-    %   y       - The vector defining the y edge of the calculation grid
-    %   z       - The vector defining the z edge of the calculation grid
-    %   s       - The direction of porpagation, s = [ydirection,zdirection]
-    %   lambda  - The wavelength
-    %   alpha   - The phase of the plane wave
-    % Output:
-    %   U       - The complex amplitude of the plane wave
-    
-    % Define the calculation grid
-    [Z, Y] = meshgrid(z, y);
-    Z = Z * s(2);
-    Y = Y * s(1);
-
-    % Define the wavenumber
-    k = (2 * pi) / lambda;
-    z
-    % Define the complex amplitude
-    A_0 = 1;
-    U = (A_0 * exp(1i * ((k * [Y + Z]) + alfa)));
-end
---
+++ skalaIntensiteter.m
@@ -0,0 +1,80 @@
+function skalaIntensiteter()
+    % Parametrar till beräkningarna
+    nLuft       = 1;
+    nGlas       = 1.6;
+    thetaLuft   = 20;
+    lambda      = 1e-6;
+    alfaLuft    = 0;
+    alfaGlas    = 0;
+    
+    % Kant vektorerna för beräknings områderna
+    y           = linspace(0, 10e-6, 800);
+    zLuft       = linspace(-5e-6, 0, 400);
+    zGlas       = linspace(0, 5e-6, 400);
+    
+    % Definera riktningsvektorn för vågen i luft
+    sLuft       = [sind(thetaLuft), cosd(thetaLuft)];
+    
+    % Definera riktningsvektorn för vågen i glas
+    thetaGlas   = asind(sind(thetaLuft) * (nLuft / nGlas));
+    sGlas       = [sind(thetaGlas), cosd(thetaGlas)];
+    
+    % Generera planen i luft och glas
+    ULuft       = planewave(y, zLuft, sLuft, lambda / nLuft, alfaLuft);
+    UGlas       = planewave(y, zGlas, sGlas, lambda / nGlas, alfaGlas);
+    
+    % Skala vågen i glas
+    [tp]    = TransCoef(sLuft, sGlas, nLuft, nGlas);
+    UGlas   = UGlas .* tp;
+    
+    % Slå samman de två vågorna
+    U       = [ULuft, UGlas];
+    %%%
+    % Genererera resultat figurer
+    %%%
+    % Beräkna fas och intensitet
+    Fas = angle(U);
+    I   = abs(U).^2;
+    
+    % Fixa vektorer till x och y axlar till figurerna
+    zAxis = [zLuft, zGlas] * 1e6;
+    yAxis = y * 1e6;
+    
+    % PLota resultatet
+    figure(1);
+    imagesc(zAxis, yAxis, Fas);
+    colormap('gray');
+    cHandel = colorbar;
+    set(cHandel.Title, 'String', '[-\pi,\pi]');
+    axis xy;
+    axis square;
+    title('Phase', 'fontsize', 18);
+    xlabel('z [\mum]', 'fontsize', 16);
+    ylabel('y [\mum]', 'fontsize', 16);
+    
+    figure(2);
+    imagesc(zAxis, yAxis, I);
+    colormap('gray');
+    Chdl = colorbar;
+    set(Chdl.Title, 'String', 'W/m^2');
+    axis xy;
+    axis square;
+    title('Intensity', 'fontsize', 18);
+    xlabel('z [\mum]', 'fontsize', 16);
+    ylabel('y [\mum]', 'fontsize', 16);
+end
+
+function [tp] = TransCoef(s1, s2, n1, n2)
+    % Denna funktion beräknar transsmisions koefficienten tp
+    % Input:
+    %   s1       - Riktningsvektorn för vågen i första mediumet
+    %   s2       - Riktningsvektorn för vågen i mediumet den går in i
+    %   n1       - Brytningsindext för första mediumet
+    %   n2       - Brytningsindex för andra mediumet
+    %
+    % Output:
+    %   tp          - Transmissionskoefficienten för p polariserat ljus
+    
+    % Beräkna tp
+    tp = (2 * n1 * s1(2)) / ((n1 * s2(2)) + (n2 * s1(2)));
+end
