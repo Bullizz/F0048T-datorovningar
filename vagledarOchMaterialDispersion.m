8c867d72054279a96c39b262ad0fdd2576a92ae9
Albin <80341227+Bullizz@users.noreply.github.com>
2025-11-26 16:37:32 +0100

    vägledar Och Material-Dispersion (inte tom)

--- vagledarOchMaterialDispersion.m
+++ vagledarOchMaterialDispersion.m
@@ -1,3 +1,54 @@
 function vagledarOchMaterialDispersion()
-
+    E       = 1e-9;         %Pulsenergi
+    a       = 4e-6;         %Kärnradie
+    n1      = 1.545;       %Brytningsindex kärna
+    n2      = 1.542;       %Brytningsindex mantel
+    lambda0 = 1.5e-6; %Vakumvåglängd
+    c       = 3e8;          %Ljusets hastighet
+    s       = 100e9;        %Spektralbandbredd
+    T       = linspace(-5e-11,5e-11,1000);  %Lokal tid
+    dndv    = 2.5e-17; %Frekvensberoendet på brytningsindex
+    delta_nu = 2*s;
+    
+    %Lägg in din kod här
+    v_0 = c / lambda0;
+    v_1 = v_0 - delta_nu;
+    v_2 = v_0 + delta_nu;
+    
+    % Nya index
+    n1_1 = n1 + dndv * (v_1 - v_0); % n1, låg frekv.
+    n1_2 = n1 + dndv * (v_2 - v_0); % n1, hög frekv.
+    
+    n2_1 = n2 + dndv * (v_1 - v_0); % n2, låg frekv.
+    n2_2 = n2 + dndv * (v_2 - v_0); % n2, hög frekv.
+    
+    [~, bm, ~, ~, ~] = singelmod(n1_1, n2_1, c / v_1, a); % frekvensen lägre än center frekvensen
+    [~, b0, ~, ~, ~] = singelmod(n1, n2, c / v_0, a);     % center frekvensens konstant
+    [~, bp, ~, ~, ~] = singelmod(n1_2, n2_2, c / v_2, a); % frekvensen högre än center frekvensen
+    
+    % Beräkna dispersionskonstanten
+    db2_dt2 = (bm - (2 * b0) + bp) / (delta_nu^2);
+    D = (1 / (2 * pi)) * db2_dt2;
+    
+    z0 = 1 / (pi * (s^2) * D);
+    sL = sqrt((D * z0) / pi);
+    
+    z1      = 0;
+    sigma1  = sL * sqrt(1 + ((z1 / z0)^2));
+    P0      = sqrt(2 / pi) * (E / sigma1) * exp(-((2 * (T.^2)) / (sigma1^2)));  % Effektprofilen vid 0km
+    
+    z2      = 1e3;
+    sigma2  = sL * sqrt(1 + ((z2 / z0)^2));
+    P1      = sqrt(2 / pi) * (E / sigma2) * exp(-((2 * (T.^2)) / (sigma2^2)));  % Effektprofil vid 1km
+    
+    z3      = 5e3;
+    sigma3  = sL * sqrt(1 + ((z3 / z0)^2));
+    P5      = sqrt(2 / pi) * (E / sigma3) * exp(-((2 * (T.^2)) / (sigma3^2)));  % Effektprofil vid 5km
+    
+    % Visa alla plottar i samma figur
+    figure(1)
+        plot(T,P0,T,P1,T,P5)
+        title('Pulses plotted together')
+        xlabel('[ps]')
+        legend('z = 0km','z = 1km','z = 5km')
 end
